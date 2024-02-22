ed1ed91160ce3a114d3bee00b31522ce4dd339d2
Vlad <vlkovalenk@gmail.com>
2024-02-22 13:29:01 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//first task
+let firstString = prompt('Give me a first string');
+let secondString = prompt('Give me a second string');
+let thirdString = prompt('Give me a third string');
+
+alert(thirdString + firstString + secondString);
+
+//second task
+let fiveDigitNumber = prompt('Give me a five digit Number');
+
+alert(fiveDigitNumber.charAt(0) + ' ' + fiveDigitNumber.charAt(1) + ' ' + fiveDigitNumber.charAt(2) + ' ' + fiveDigitNumber.charAt(3) + ' ' + fiveDigitNumber.charAt(4))
